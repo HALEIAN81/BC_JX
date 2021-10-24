608efe454fd3f3369ef4cad1945b5078148cdce0
Hale Verde <haleverde2020@gmail.com>
2021-10-24 11:25:26 -0700

    added stage 2 code with isUservalid function

--- script.js
+++ script.js
@@ -16,16 +16,25 @@ var newsFeed = [
     }
 ];
 
-var userNamePrompt = prompt("What is your username sir or maam?");
-var passwordPrompt = prompt("What's your password stupid?");
+function isUserValid(username, password) {
+    for (var i=0; i < database.length; i++) {
+        if(database[i].username === username && 
+            database[i].password === password) {
+            return true;
+            }
+    }
+    return false;
+}
 
-function signIn(user, pass) {
-    if (user === database[0].username &&
-        pass === database[0].password) {
+function signIn(username, password) {
+    if (isUserValid(username, password)) {
         console.log(newsFeed);
     } else {
         alert("Sorry, that is not the correct password and username combination!!!");
     }
 }
+
+var userNamePrompt = prompt("What is your username sir or maam?");
+var passwordPrompt = prompt("What's your password stupid?");
 
 signIn(userNamePrompt, passwordPrompt);
